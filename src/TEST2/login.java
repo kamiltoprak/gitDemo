e9d6fb9b06b594e1669d2e5830d42b6fb90b6e32
kamiltoprak <ktoprak@gmail.com>
2020-08-01 10:56:26 -0400

    july 31 30

--- src/TEST2/login.java
+++ src/TEST2/login.java
@@ -3,5 +3,6 @@ package TEST2;
 public class login {
     public static void main(String[] args) {
         System.out.println("login");
+        System.out.println("hi");
     }
 }
--- src/TEST2/logout.java
+++ src/TEST2/logout.java
@@ -3,5 +3,6 @@ package TEST2;
 public class logout {
     public static void main(String[] args) {
         System.out.println("logout");
+        System.out.println("How  are you");
     }
 }
