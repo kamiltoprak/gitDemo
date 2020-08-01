26925783efb4be21af11238433b10b925008802d
kamiltoprak <ktoprak@gmail.com>
2020-08-01 10:54:42 -0400

    july 31 20

---
+++ src/TEST2/login.java
@@ -0,0 +1,7 @@
+package TEST2;
+
+public class login {
+    public static void main(String[] args) {
+        System.out.println("login");
+    }
+}
---
+++ src/TEST2/logout.java
@@ -0,0 +1,7 @@
+package TEST2;
+
+public class logout {
+    public static void main(String[] args) {
+        System.out.println("logout");
+    }
+}
